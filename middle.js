6db115a9086449f604484815a41ff2a3d0d04fab
Zhao Wang <wangz2266@gmail.com>
2020-02-04 16:20:58 -0800

    add middle.js

---
+++ middle.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("😄😄😄Assertion Passed: ", arr1, " === ", arr2);
+  } else {
+    console.log("😓😓😓Assertion Failed: ", arr1, " !== ", arr2);
+  }
+};
+
+const middle = function(arr) {
+  let l = arr.length;
+  let res = [];
+  if (l <= 2) {
+    return res;
+  }
+  if (l % 2 == 1) {
+    res.push(arr[Math.floor(l / 2)]);
+  } else {
+    res.push(arr[l / 2 - 1], arr[l / 2]);
+  }
+  return res;
+};
+
+assertArraysEqual(middle([3, 4, 5, 6, 7, 8]), [5, 6]);
+assertArraysEqual(middle([4, 5, 6, 7, 8]), [5, 6]);
+assertArraysEqual(middle([4, 5, 6, 7, 8]), [6]);
