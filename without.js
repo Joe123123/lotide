a978e2f477d529783f74a307792ee7b1a761ee40
Zhao Wang <wangz2266@gmail.com>
2020-02-04 16:42:30 -0800

    add without.js

---
+++ without.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("😄😄😄Assertion Passed: ", arr1, " === ", arr2);
+  } else {
+    console.log("😓😓😓Assertion Failed: ", arr1, " !== ", arr2);
+  }
+};
+
+const without = function(sourceArr, itemToRemoveArr) {
+  let res = [];
+  for (item of sourceArr) {
+    if (!itemToRemoveArr.includes(item)) {
+      res.push(item);
+    }
+  }
+  return res;
+};
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
