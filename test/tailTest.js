3787ef0c627a881a7b9315886a9474b942e69659
Zhao Wang <wangz2266@gmail.com>
2020-02-10 15:21:59 -0800

    modified test folder

---
+++ test/tailTest.js
@@ -0,0 +1,21 @@
+const tail = require("../tail");
+const assert = require("chai").assert;
+
+describe("Tail function:", () => {
+  it("Returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+  it("Returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+  it("Returns [] for ['Hello']", () => {
+    const result = tail(["Hello"]);
+    assert.deepEqual(result, []);
+  });
+  it("Returns [] for []", () => {
+    const result = tail([]);
+    assert.deepEqual(result, []);
+  });
+});
