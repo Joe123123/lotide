a1e9d454bde4458401c7693cc4b61efd8d8072dd
Zhao Wang <wangz2266@gmail.com>
2020-02-10 17:48:27 +0000

    use module.exports and require, add test folder

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😄😄😄Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😓😓😓Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([6]), 5);
+module.exports = head;
